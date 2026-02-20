4acd2f0c45d98723b748c7a7e14c26123d49b337
Stephen Walsh <spwalsh25@gmail.com>
2026-02-20 12:14:36 -0600

    Manual deploy via subtree

--- README.md
+++
@@ -1,3 +0,0 @@
-# NUSO-Website
-
-A public website for Northwestern University Science Olympiad (NUSO) with information about the invitational, registration, and past competitions.
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Home } from "./Home";
-export { default as About } from "./About";
-export { default as Invitational } from "./Invitational";
-export { default as Tournaments } from "./Tournaments";
-export { default as Volunteer } from "./Volunteer";
-export { default as Contact } from "./Contact";
-export {default as PageLayout} from "./PageLayout";
